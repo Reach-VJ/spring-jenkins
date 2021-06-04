1ad1392aab092119efc22ce938ba86d8e8ffa34b
Vijaya Banka <vijay.bhaskar@macys.com>
2021-06-03 21:05:41 -0700

    Revert "Second commit"
    This reverts commit 1283fd21a0df5bdb8f71181706580889234db969.

--- src/test/java/com/reachingskies/spring/jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/reachingskies/spring/jenkins/SpringJenkinsApplicationTests.java
@@ -18,7 +18,6 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
-		logger.info("Test case executing second time...");
 		assertEquals(true,true);
 	}
 
